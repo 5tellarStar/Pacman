9d82c529a3951b0168a1922b71af3c127b939050
22skogus <22skogus@303-H51>
2024-03-12 15:18:42 +0100

    Add project files.

---
+++ Maze2/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Maze2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            tick = new System.Windows.Forms.Timer(components);
+            mode = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // tick
+            // 
+            tick.Enabled = true;
+            tick.Interval = 500;
+            tick.Tick += timer1_Tick;
+            // 
+            // mode
+            // 
+            mode.Enabled = true;
+            mode.Interval = 1000;
+            mode.Tick += mode_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(914, 600);
+            DoubleBuffered = true;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tick;
+        private System.Windows.Forms.Timer mode;
+    }
+}
